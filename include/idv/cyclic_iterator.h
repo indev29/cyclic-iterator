6a6fb848dca2814aab1cd4a33d5ff5e402dbc4cc
indev <indev.src@gmail.com>
2022-02-26 18:44:37 +0700

    chore: add license

--- include/idv/cyclic_iterator.h
+++ include/idv/cyclic_iterator.h
@@ -1,3 +1,9 @@
+
+//          Copyright Maxim Kolesnikov 2022.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef IDV_CYCLIC_ITERATOR_H
 #define IDV_CYCLIC_ITERATOR_H
 
